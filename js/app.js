c145fa22add0ec0da9d5f9ff9e5ffbd97f3d83ab
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-04-16 01:26:34 -0500

    feat: modified global variables and added a update score function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 // ====================== GLOBAL DOM / VARIABLES ======================= //
-const game = document.getElementById('game');
+let block = document.getElementById("block");
+let hole = document.getElementById("hole");
+let circle = document.getElementById("circle");
+let jumping = 0;
+let counter = 0;
+let movementInterval;
+let rectangleInterval;
 
+// ==================Function to update the score=========================//
+function updateScore() {
+    counter++;
+    document.getElementById("score").textContent = counter; // Update the HTML score
+}
