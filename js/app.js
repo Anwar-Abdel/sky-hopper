1dae185a9bc90455483d28b7c2755710e39f901d
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-04-15 22:08:39 -0500

    feat: adjusted html and css content. Removed js parts to re do

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
 // ====================== GLOBAL DOM / VARIABLES ======================= //
 const game = document.getElementById('game');
-let background = document.getElementById('background');
-let bird = document.getElementById('bird');
-let topPipe = document.getElementById('top-pipe');
-let bottomPipe = document.getElementById('bottom-pipe');
 
-bottomPipe = 65;
-
-
-// ====================== CANVAS ======================= //
-game.setAttribute('height', getComputedStyle(game)['height']);
-game.setAttribute('width', getComputedStyle(game)['width']);
